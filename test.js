2a77a316e05c68290b68894c91628aa06b1cc67e
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-01-14 22:58:22 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const adminprefix = "o";
-const developers = ["440081527909515265" , "474200581163057153"];
+const adminprefix = "i";
+const developers = ["450781395481001996" , "474200581163057153"];
 
 
 client.on('ready', () => {
@@ -25,23 +25,11 @@ client.on('message', message => {
       message.channel.send(``)
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/Randy");
+    client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
       message.channel.send(``)
   }
 });
 
-
-client.on('message', msg => {
-  if (msg.content === 'مليكانو') {
-    msg.channel.send(`\`\`\`عيون مليكانو إذا ما رديت كلمني إنستا jor.io\`\`\``);
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '<@440081527909515265>') {
-    msg.channel.send(`\`\`\`عيون مليكانو إذا ما رديت كلمني إنستا jor.io\`\`\``);
-  }
-});
 
 
 client.login(process.env.BOT_TOKEN);
